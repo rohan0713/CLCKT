16b1772b061da97e268c965b9ce8d41ecfcf2399
rohan.ramola <rohan.ramola@india.nec.com>
2026-04-28 19:35:07 +0530

    Initial commit

---
+++ app/src/main/java/app/aura/clckt/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.aura.clckt
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.aura.clckt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
